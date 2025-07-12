c1e77bb96dc8a9219becf6a44a2c5db572e5e00b
Quách Thành Long <stephensouth1307@gmail.com>
2025-07-12 15:44:47 +0700

    add

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -5,3 +5,15 @@ import { AssessmentForm } from "@/components/assessment-form"
 export default function AssessmentPage() {
   return <AssessmentForm />
 }
+
+/* src/app/assessment/page.tsx
+import { AssessmentForm } from "@/components/assessment-form"; // Giả sử bạn lưu form vào đây
+
+export default function TakeAssessmentPage() {
+  return (
+    <main>
+      <AssessmentForm />
+    </main>
+  );
+}
+  */
